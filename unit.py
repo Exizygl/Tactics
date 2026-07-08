506cb8b7e305c8a1afe91e92abc52192d236a210
Exizygl <exizelyglaya@gmail.com>
2026-07-08 19:55:25 +0200

    feat: create class unit

---
+++ unit.py
@@ -0,0 +1,7 @@
+ def __init__(self, name, sprite, x, y):
+        self.name = name
+        self.sprite = sprite
+        self.movement_type = movement_type
+
+        self.x = x
+        self.y = y
